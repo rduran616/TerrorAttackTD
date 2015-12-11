c99575e7abfedeca53ff10011864db21e18efc96
CaniculeGame <florian.raffalli@gmail.com>
2015-12-11 19:00:45 +0100

    j

--- core/src/com/mygdx/game/Jeu.java
+++ core/src/com/mygdx/game/Jeu.java
@@ -7,18 +7,41 @@ public class Jeu extends StateMenu
 {
 	GlobalValues values_;		
 	StateMEnuEnum selection_;
+	StateJeu jeu_[];
+	StateJeuEnum etat_jeu_;
+	
 	
 	public Jeu()
 	{
+		//initialisation des variables
 		selection_ = StateMEnuEnum.JEU;
 		values_ = GlobalValues.getInstance();
+		
+		//initialisation du jeu
+		etat_jeu_ = StateJeuEnum.INTRO;
+		jeu_ = new StateJeu[4];
+		if(jeu_ != null)
+		{
+			jeu_[0] = new TdIntro();
+			jeu_[1] = new TdPause();
+			jeu_[2] = new TdJeu();
+			jeu_[3] = new TdFin();
+		}
+
 	}
 
 	@Override
 	public StateMEnuEnum changer_Etat() 
 	{
 
-		//affichage menu
+		// choix de l'etat et action en focntion de l'état
+		
+		
+		
+		//dessin du jeu
+		
+		
+
 		
 		//Changer de menu
 		if(selection_ != StateMEnuEnum.JEU)
