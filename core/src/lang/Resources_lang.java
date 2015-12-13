7eb0efe8574354422901bd376ecf8c1eb2d3ff6e
rduran616 <cremy83@laposte.net>
2015-12-13 17:51:42 +0100

    tit ajout

--- core/src/lang/Resources_lang.java
+++ core/src/lang/Resources_lang.java
@@ -3,19 +3,28 @@ package lang;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.swing.JFrame;
+
 public class Resources_lang {
 
+	private JFrame frame;	
+	Locale locale;
+	ResourceBundle res;
+	
+	
+	//locale = new Locale("fr","");	
+		
+	//locale = new Locale("en","");
+		
 	public Resources_lang()
 	{
-				
-		Locale locale;
-		
-		
-		locale = new Locale("fr","");
-		
-		
-		locale = new Locale("en","");
-		
+		initialize();
+	}
+	
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
 	}
